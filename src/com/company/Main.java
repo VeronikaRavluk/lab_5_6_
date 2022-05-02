b4b6b82e0db63831749ee4671f6b1978e31dd659
Veronika Ravluk <veronikaravluk6@gmail.com>
2022-05-02 21:05:31 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        {
+            //№1
+            System.out.println("Завдання № 1");
+            int a;
+            a = 80 + 20;
+            System.out.println("Рузультат : " + a);
+            int b = a - 80;
+            System.out.println("Рузультат : " + b);
+            int c = a / b;
+            System.out.println("Рузультат : " + c);
+            int d = c % 2;
+            System.out.println("Рузультат : " + d);
+        }
+        {
+            //№2
+            System.out.println("Завдання № 2");
+            int a = 15;
+            int b = a%10;
+            int c = (a/10)%10;
+            System.out.println("Суму цифр двозначного числа " +a+"="+(b+c));
+        }
+        {
+            //№3
+            System.out.println("Завдання № 3");
+            int a;
+            a = 306+99;
+            System.out.println("Рузультат : "+a);
+            int b = a-64;
+            System.out.println("Рузультат : "+b);
+            int c = a/b;
+            System.out.println("Рузультат : "+c);
+            double d = c;
+            System.out.format("%3f",+ d );
+
+        }
+
+    }
+
+}
